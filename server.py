938d603d91a565035ce86e82fd0470ad26ba469e
fksay <132195945+fksay@users.noreply.github.com>
2025-07-05 22:07:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify 
 import requests
 import os
 
@@ -46,4 +46,5 @@ def transcribe_audio():
         return jsonify({'error': error}), response.status_code
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get('PORT', 5000))  # Render ke liye dynamic port le raha hai
+    app.run(host="0.0.0.0", port=port)
